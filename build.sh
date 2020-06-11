88b86fee0d092acc44b790e25a9941aebc46f9c7
peterjskaltsis <pjs300300@gmail.com>
2020-06-11 22:51:34 +1000

    remove optimisation (temp)

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 # Files to be compiled/output.
 elm="src/Main.elm"
-js="public/main.js"
+js="public/main.min.js"
 min="public/main.min.js"
 
 # Formatting.
@@ -12,17 +12,17 @@ color_reset="\x1b[0m"
 module=">$color_green elm-build-tool$color_reset -"
 
 # Compile elm code.
-echo $module "compiling $color_cyan$elm$color_reset[0m to $color_cyan$js$color_reset ..."
+echo $module "compiling $color_cyan$elm$color_reset to $color_cyan$js$color_reset ..."
 elm make $elm --optimize --output=$js
 
 # Optimise elm code.
-echo $module "optimising $color_cyan$js$color_reset to $color_cyan$min$color_reset ..."
-npm run uglify $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | uglifyjs --mangle --output=$min
+# echo $module "optimising $color_cyan$js$color_reset to $color_cyan$min$color_reset ..."
+# npm run terser -- $js --compress 'pure_funcs="F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9",pure_getters,keep_fargs=false,unsafe_comps,unsafe' | npm run terser -- --mangle --output $min
 
 # Success!
-echo $module "compression stats:"
-echo "\tInitial size: $(cat $js | wc -c) bytes  ($js)"
-echo "\tMinified size:$(cat $min | wc -c) bytes  ($min)"
-echo "\tGzipped size: $(cat $min | gzip -c | wc -c) bytes"
+# echo $module "compression stats:"
+# echo "\tInitial size: $(cat $js | wc -c) bytes  ($js)"
+# echo "\tMinified size:$(cat $min | wc -c) bytes  ($min)"
+# echo "\tGzipped size: $(cat $min | gzip -c | wc -c) bytes"
 
 echo $module "elm program $color_cyan$elm$color_reset successfully compiled."
